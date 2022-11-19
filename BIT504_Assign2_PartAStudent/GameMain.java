38d0d1407ab2ca106f50555146044aabbf5f796f
tabingtons <harry.tavis.brown@icloud.com>
2022-11-20 12:28:49 +1300

    Added MouseListener
    Added the event listener to GameMain so that it can register when the
    mouse is clicked.

--- BIT504_Assign2_PartAStudent/GameMain.java
+++ BIT504_Assign2_PartAStudent/GameMain.java
@@ -37,9 +37,9 @@ public class GameMain extends JPanel implements MouseListener{
 
 	/** Constructor to setup the UI and game components on the panel */
 	public GameMain() {   
-		
-		// TODO: This JPanel fires a MouseEvent on MouseClicked so add required event listener to 'this'.          
-	    
+	         
+		// Listener to register mouse events
+        addMouseListener(this);
 	    
 		// Setup the status bar (JLabel) to display status message       
 		statusBar = new JLabel("         ");       
