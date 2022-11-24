f560b5e8f074b9bdc7694accb61ffc7b96c254ad
tabingtons <harry.tavis.brown@icloud.com>
2022-11-25 12:53:26 +1300

    Initialise each cell on the board
    Call Cell constructor from Board constructor to create a cell for each
    row and column intersection. Cell constructor identifies the row and col
    of each cell and sets to state Empty.

--- BIT504_Assign2_PartAStudent/Cell.java
+++ BIT504_Assign2_PartAStudent/Cell.java
@@ -12,11 +12,14 @@ public class Cell {
 	/** Constructor to initialise this cell with the specified row and col */
 	public Cell(int row, int col) {
 		
-		// TODO: Initialise the variables row, col 
+		// TODO
+		//Initialise the variables row, col 
+		this.row = row;
+		this.col = col;
 		
-		
-		//TODO: call the method that sets the cell content to EMPTY
-		 
+		//TODO
+		//call the method that sets the cell content to EMPTY
+		clear();
 	}
 	
 
@@ -44,7 +47,9 @@ public class Cell {
 	/** Set this cell's content to EMPTY */
 	public void clear() {
 		
-		// TODO: Set the value of content to Empty (Remember this is an enum)
+		// TODO
+		//Set the value of content to Empty (Remember this is an enum)
+		this.content = Player.Empty;
 
 		
 	}
